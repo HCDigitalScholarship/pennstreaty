c39996c2b7ccff97ff5151c2b7c7837b3f578dc3
root <root@pennstreaty.haverford.edu>
2016-09-22 10:04:07 -0400

    committing files related to /mapgallery

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+    
+    function reHeight() {
+        $('.my-grid > li').each(function() {
+                $(this).height($(this).width() * 0.65);
+        });
+    }
+    
+    reHeight();
+    
+    $(window).resize(reHeight);
+    
+});
