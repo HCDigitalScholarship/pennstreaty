df90333b90cfd44e18825d3d98b932347e2d7387
root <root@pennstreaty.haverford.edu>
2017-02-17 11:20:27 -0500

    changing this file back to original

--- QI/management/commands/new_XML_to_HTML.py
+++ QI/management/commands/new_XML_to_HTML.py
@@ -379,11 +379,11 @@ def xml_to_html(xml_file):
       with open('sample.txt', "w") as f:
 	      for html in html_list:
 		if index<10:
-			sindex='_00'+str(index-1)
+			sindex='_00'+str(index)
 		elif index<100:
-			sindex='_0'+str(index-1)
+			sindex='_0'+str(index)
 		elif index<1000:
-			sindex='_'+str(index-1)
+			sindex='_'+str(index)
 		else:
 			print "index got too high (over 999)! Naming convention off"
 			sindex=str(index)
