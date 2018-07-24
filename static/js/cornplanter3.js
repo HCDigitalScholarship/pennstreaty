bd4a691573b5f88054e1024df95064a85172763d
shufanXia <sxia1@haverford.edu>
2018-07-24 18:35:20 +0000

    remove openseadragon from conplabter3.js

--- static/js/cornplanter3.js
+++ static/js/cornplanter3.js
@@ -35,7 +35,8 @@ function inPage(newID){
     }
     return newImgUrl;
 }
-$(document).ready(function() {
+
+/*$(document).ready(function() {
     //initPage(pageID);
     if (pageID < 1 && pageID > lastpage) {
 	alert("Page out of range.");
@@ -44,11 +45,7 @@ $(document).ready(function() {
     	var viewer = OpenSeadragon({
             id: "openseadragon1",
             prefixUrl: "//openseadragon.github.io/openseadragon/images/",
-//	    tileSources: "/srv/QI/static/img/Misc_mss_1791_06_02_001.dzi",
-//            tileSources: "http://104.131.45.60/img/Misc_mss_1791_06_02_001.dzi"
-	    tileSources: "http://159.203.172.178/img/"+ inPage(pageID)+ ".dzi"
-//	    tileSources: duomo
-/*            showNavigator: true,
+            showNavigator: true,
             navigatorPosition: "ABSOLUTE",
             navigatorTop: "40px",
             navigatorLeft: "4px",
@@ -56,7 +53,7 @@ $(document).ready(function() {
             navigatorWidth: "90px",
             sequenceMode: true,
             showReferenceStrip: true,
-            referenceStripScroll: 'horizontal',*/
+            referenceStripScroll: 'horizontal',
     	});
     }
     // This controls what happens when someone uses the close button.
@@ -79,4 +76,4 @@ $(document).ready(function() {
         initPage(pageID - 1);
 	location.reload();
     });
-});
+});*/
