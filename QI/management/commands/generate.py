3c22350c1bc246f9cb8a2fd6ada5026d44e45a65
shufanXia <sxia1@haverford.edu>
2018-07-24 18:07:35 +0000

    upadate generate.py for storymap to python3, fix img_url and text display

--- QI/management/commands/generate.py
+++ QI/management/commands/generate.py
@@ -1,14 +1,10 @@
-##### move file SW_GH1804.xml to static/xml
-##### python manage.py generate SW_GH1804
-
-##### Comment numbers correspond to additional explanation in the documentation
-
 
 from django.core.management.base import BaseCommand, CommandError
 
 #1
 import os
 from xml.etree.ElementTree import ElementTree
+
 from xml.etree.ElementTree import Element
 import xml.etree.ElementTree as etree
 import json
@@ -27,14 +23,14 @@ class Command(BaseCommand): #2
 	
 	def handle(self, *args, **options):
 		for xml_file in options['xml_file']: #3
+		#this for loop allows you generate storymaps for multiple xml files by running python manage.py generate file1 file2
 			#4
 			file_name = 'static/xml/' + xml_file + '.xml'
 			workbook = xlrd.open_workbook('static/xls/TEI people, places, orgs.xlsx')#LOCATION
 			worksheet = workbook.sheet_by_name('Places')#LOCATION
 			tree = etree.parse(file_name)
 			root = tree.getroot() 
-			#5
-			#namespace - needs to be put before every element tag 
+
 			ns = ""
 			if root.tag[0] == "{":
 				x = 1
@@ -42,7 +38,8 @@ class Command(BaseCommand): #2
 			while root.tag[x-1] != "}":
 				ns += root.tag[x]
 				x += 1
-			#6
+			#line 35-41 are necesssary to keep, although I don't understand what id namespace here. It is inherited from old version of generate.py 	
+
 			title = ""
 			titlelist =[]
 			for a in root.iter(ns + "titleStmt"):
@@ -52,6 +49,7 @@ class Command(BaseCommand): #2
 					for child in x:
 						if child.tag == ns + "title":
 							title = child.text
+							#get node value under "<title>"
 			title_fin = title
 			if title.endswith("Version"):
 				title_fin = title_fin.replace("\n","")
@@ -59,7 +57,10 @@ class Command(BaseCommand): #2
 				title_fin = title_fin.replace("Electronic","")
 				title_fin = title_fin.replace(": ", "")
 				title_fin = title_fin.replace("  ","")
+				#delete ":Electronic:Version" from the text under <title> in xml
+			print (title_fin)
 			#7
+
 			description = ""
 			desc_list = []
 			for b in root.iter(ns + "projectDesc"):
@@ -68,156 +69,118 @@ class Command(BaseCommand): #2
 				for b in root.iter(ns + "projectDesc"):
 					for child in b:
 						if child.tag == ns + "p":
-							description = child.text		
-			#8			
-			#Separating entries and the text of entries - making a list for both
-			div_text = []
-			#Getting everything as text for each div, so I can later take out tags
-			divs = root.iter(ns + 'div')
-			#Making a list of "div type=entry" elements.
+							description = child.text	
+			print (description[:100]+"...")
+
 			entries = []
-			for div in divs:
+			for div in root.iter(ns + 'div'):
 				if div.get('type') == 'entry':
 					entries.append(div)
-					div_text = div_text + [etree.tostring(div)]
-			#div_text is a list of strings, one for each div type= entry
-			#9
-			# Making the text nicer -- removing <> xml tags
-			final_text = []
-			#final_text is a list of div strings without tags
-			for string in div_text:
-				new = ""
-				a = 0
-				b=0
-				while(a < len (string)):
-					a_init = a
-					if string[a]== "<":
-						b = a 
-						while (string[b] != ">" and b < len(string)):
-							b = b + 1
-						a = b+1
-					else:
-						new = new + str(a_init)
-						a = a+1
-				final_text = final_text + [new]
-				#final_text is a list of div strings without tags
-			#10
-			#Getting page breaks:
-			objects = [0]*len(entries)	
-			#creates list of zeroes to later hold pseudo-json objects	
-			pages = []
-			for f,e in enumerate(entries):
+			print (entries)
+			# storymap only create slides for "type=entry". We get all divs with"type = entry" first.
+
+			objects = []# createing a list that will hold each slides of storymap, and this object is stored as json at last
+			pages = []# hold page numbers for image caption later
+			tei_ids=[]# hold tei_id to create img_url
+			for f, e in enumerate(entries):
+				#f is intger, e are 'div type=entry' elements
+				div_text = ""
+
 				for child in e:
-					page = []
-					if child.tag == ns + "pb":
-						page = page + [child.get("n")]
-					else:
-						for ch in child:
-							if ch.tag == ns + "pb":
-								page = page + [ch.get("n")]
-							else:
-								for c in ch:
-									if c.tag == ns + "pb":
-										page = page + [c.get("n")]		
-				pages.append(page)
-			####getting first possible page for overview slide
-			#11
-			first_page = 0
-			all_pb = []
-			for c in root.iter(ns + "pb"):
-				all_pb.append(c.get("n"))
-			#12
-			fp_new = "" #new first page
-			if "[" in all_pb[0] or "]" in all_pb[0]:
-				for s in range(0,len(all_pb[0])):
-					if all_pb[0][s] != "[" and all_pb[0][s] != "]":
-						fp_new = fp_new + all_pb[0][s]	
-			else:
-				fp_new = all_pb[0]
-			if pb_start(entries, ns) == True:
-				pages_final = get_pages_alt(pages)
-				first_page = int(fp_new)
-			else:
-				pages_final = get_pages(pages)
-				first_page = int(fp_new) - 1
-			#13
-			###OBJECTS DEFINED HERE - Getting date and location data
-			for f,e in enumerate(entries): 
-			#f is int, e are "div type=entry" elements
-				for child in e:
+					if child.tag == ns + 'p':
+					 #each entry contains multiple "<p>", iterate within one entry to get all text
+						text = etree.tostring(child, method='text')
+						#tostring method will convert xml to string, but it will keep some tags
+						text = text.decode('utf-8')
+						text = text.replace('\n', '')
+						text = text.replace('\\xe2\\x80\\x94','-')
+						text = text.replace('\\xe2\\x80\\x99',"'")
+						# line 94-97 take out tags and replace some of unicode characters
+						div_text = div_text+text
+
+						for subelement in child:
+							if subelement.tag == ns + 'pb':
+								page = subelement.get('n')
+								tei_id = subelement.get('facs')#use 'facs' value to get tei_id. There are issues with this. Read wiki
+								pages.append(page)
+								tei_ids.append(tei_id)
+
 					if child.tag == ns + "dateline":
+						# <dateline><date when="xxx">xxx</date>.<placeName key="zz">yyy</placeName></dateline>, we can get date and place in <dateline> element
 						dateline = child
 						for child in dateline:
-							if child.tag == ns +"placeName":
+							if child.tag == ns + "placeName":
 								place = child.get("key")
-							if child.tag == ns+"date":
+							if child.tag == ns + "date":
 								date = child.get("when")
-				'''
-				teiId = place #LOCATION
-				print (place, "Place")
-				found = False#LOCATION
-				row = 0#LOCATION
-				while (worksheet.cell(row, 0).value != xlrd.empty_cell.value and found == False):#LOCATION
-					#print worksheet.cell(row, 0).value, "value"
-					if worksheet.cell(row, 0).value == teiId:#LOCATION
-						found = True#LOCATION
-					else:#LOCATION
-						row +=1#LOCATION
-				lat = worksheet.cell(row, 4).value#LOCATION
-				lon = worksheet.cell(row, 5).value#LOCATION
-				'''
-				#print place
+				#print ('div_text is:', div_text)
+				print ('last page in pages is: ', pages[-1:])
+				print ('last tei_id in pages is: ', tei_ids[-1:])
+
 				
+				img_caption = page_repr(pages[-1:])
+				img_url = media_url_repr(tei_ids[-1:])
+				print ('img_caption after loop:', img_caption)
+				print ('img_url after loop:', img_url)
+				
+				#get a list of place(should be one place only) in this entry
 				dj_place_list = Place.objects.filter(id_tei=place)
-				print (dj_place_list)
-				dj_place="Blue"
-				if dj_place_list==None or dj_place_list==list() or len(dj_place_list)==0: #This stopped working overnight. Just want to know if query was unsuccessful				
+				print ("dj_place_list:: \n", dj_place_list, len(dj_place_list))
+				
+
+				if dj_place_list==None or dj_place_list==list() or len(dj_place_list)==0: 
+				#check if places from the manuscript are in database			
 					print (place, "not found in database \n")
 					continue
+
 				else:
 					for i in dj_place_list:
 						if i.latitude != None:
-							dj_place = i
+							dj_place = i 
 							break
+							 #if it passes the if statement, skip the loop 
 						else:
 							dj_place = i
-						print ("DID THIS")
-				lat = dj_place.latitude
-				lon = dj_place.longitude
-				#filler latitude/longitude
-				if lat == '':#LOCATION
-					print ("No lat long for",place,"I set this to lat to 42 and lon to 83. \n")
-					lat = 42 #LOCATION
-				if lon == '':#LOCATION
-					lon = 83#LOCATION
-				else:
-					print (place, "had lat, lon:",lat,lon,"\n")
-				try:
-					float(lat)
-				except ValueError:
-					print ("People are bad at entering data, lat for this is:", lat)
-					lat = 42
-					print ("set lat to 42")
-				try:
-					float(lon)
-				except ValueError:
-					print ("People are bad at entering data, lon for this is:", lon)
-					lon = 83
-					print ("set lon to 83")					
+							print ("i.latitude is 'None'")
+
+					lat = dj_place.latitude
+					lon = dj_place.longitude
+
+					if lat =="" or lon== "":
+						print ("No lat or lon for",place,"I set this to lat to 42 and lon to 83. \n")
+						lat = 42 #LOCATION
+						lon = 83#LOCATION
+					else:
+						print (place, "had lat, lon:",lat,lon,"\n")
+					try:
+						float(lat)
+					except ValueError:
+						print ("People are bad at entering data, lat for this is:", lat)
+						lat = 42
+						print ("set lat to 42")
+					try:
+						float(lon)
+					except ValueError:
+						print ("People are bad at entering data, lon for this is:", lon)
+						lon = 83
+						print ("set lon to 83")			
 				#Important: the spreadsheet is North and West, so you have to make it negative if you dont want to end up in Tajikistan
-				objects[f] = {"location":{"lat" : float(lat), "lon": -float(lon)}, "text":{"headline": date_repr(date), "text" : final_text[f]}, "media":{"url": "https://ds-omeka.haverford.edu/qi/files/fullsize/4e0fe986a9716456496045d5fe4f608f.jpg", "caption" : page_repr(pages_final[f])} }
-			#14
-			#ADDING OVERVIEW INFORMATION
-			final = [{"type": "overview", "text":{"headline": title_fin, "text" : description},"media":{"url": "https://ds-omeka.haverford.edu/qi/files/fullsize/4e0fe986a9716456496045d5fe4f608f.jpg", "caption" : "First Page"} }] + objects
-			#Adding JSON Outside bits - 3/29/2016 
-			final2 = {"calculate_zoom": False, "storymap": {"call_to_action": True, "map_type": "stamen:toner-lite", "map_as_image": False, "slides": final}}
-			data = final2
-			#15
+				object = {"location":{'lat':float(lat), 'lon':-float(lon)}, "text":{"headline":date_repr(date), "text":div_text},"media":{"url":img_url,"caption":img_caption}}
+				objects.append(object)
+
+			cover = {"type":"overview", "text":{"headline":title_fin, "text":description}}
+			objects.insert(0, cover)
+
+			map = {"calculate_zoom":False, "storymap":{"call_to_action":True, "map_type":"statemen:toner-lite","map_as_image":False, "slides":objects}}
+			data = map
+			
+			#write json data into a json dile 
 			with open('static/json/' + xml_file + '.json', 'w') as outfile:
 				json.dump(data, outfile, sort_keys=True)
-			#16	
+			
+			#write html template for the storymap
 			html_str = """{% load staticfiles %}
-<!doctype html>
+			<!doctype html>
 <html class="no-js" lang="en">
   <head>
     {% include 'top-links.html' %}
@@ -226,28 +189,20 @@ class Command(BaseCommand): #2
     
   </head>
   <body>
-	<header>
-          <div class = "overlay-container">
-            <img src="{% static "img/PennsTreaty-West.jpg" %}" alt="Header img" />
-              <div class = "overlay1 show-for-large-up">
-               <!--  <strong>Beyond Penn's Treaty</strong> -->
-              </div>
-        </div>
-   
-    </header>
+	{% include 'header.html' %}
    <div id = "row1">
   <!--     Navigation Menu Below:  -->
     {% include 'nav.html' %}
   </div>
-	<div id = "story1" style = "background-color: #0F6AB3;">
-	   <div class = "row">
+	<div id = "story1" style = "background-color: #F0F8FF;">
+	   <div>
 	      <br />
-	      <h2 class = "text-center" style = "font-family: 'Alegreya Sans SC'; font-weight: 400; color: white"> StoryMap for """+ xml_file + """</h2>
+	      <h2 class = "text-center" style = "font-family: 'Alegreya Sans SC'; font-weight: 400; color: black"> StoryMap for """+ xml_file + """</h2>
 	        <!-- The StoryMap container can go anywhere on the page. Be sure to 
     specify a width and height.  The width can be absolute (in pixels) or 
     relative (in percentage), but the height must be an absolute value.  
     Of course, you can specify width and height with CSS instead -->
-    <div id="mapdiv" style="width: 100%; height: 600px;"></div> 
+    <div id="mapdiv" style="width: 90%; height: 600px; margin:auto"></div> 
     <br />
     <br />
 	  </div>  
@@ -274,36 +229,43 @@ class Command(BaseCommand): #2
   
   </body>
 </html>""" 
-			html_file = open('templates/story_maps/' +xml_file + '.html', 'w')
+			html_file = open('templates/story_maps'+xml_file+'.html','w')
 			html_file.write(html_str)
-			html_file.close() #creates html template
-			#17
+			html_file.close()
+			#create html template
+
 			#adds to list of xml ids to be made into urls
 			file_names = 'static/xml/xml_file_names.xml'
 			tree = etree.parse(file_names)
 			root = tree.getroot()
-			list_of_files = []
+			list_of_files=[]
 			for child in root:
 				list_of_files.append(child.text)
 
-			new_file = False #Use this existing code to detirmine if I need to add a new file to storymapdir
+			new_file = False
+
 			if xml_file not in list_of_files:
-				x = etree.SubElement(root, 'file')
+				x = etree.SubElement(root,'file')
 				x.text = xml_file
 				tree.write('static/xml/xml_file_names.xml')
 				new_file = True
-			new_file = True #delete this when ready, just for testing
+			#to check if this storymap has already existed	
+			#new_file = "True"#delete this when I am done with storymap!!!!
+
 			if new_file:
-				mynewfile=""
+				#if it is a new file, we need to make a link to it on 'list_of_storymap.html'.
+				newfile=""
 				with open('templates/list_of_storymaps.html', 'r+') as f:
 					html_as_string=f.read()
-					print (html_as_string)
+					
 					soup = BeautifulSoup(html_as_string, 'html.parser')
+					#BeaurifulSoup is a tool that parser html
 					sml = soup.find(id='storymaplist')
 					links = soup.find_all('a')
 					newtag= soup.new_tag('a', id='SMLink', href=xml_file)
 					atag= soup.new_tag('a', href=xml_file)
-					imgtag= soup.new_tag('img', src='../static/img/'+xml_file+'_001.jpg')
+					imgtag= soup.new_tag('img', src='../static/img/'+xml_file+'1797.jpg')
+					#url in line 267 need to be replaced with correct url manually in list_of_storymaps.html
 					litag = soup.new_tag('li', id='SMListitem')
 					divtag = soup.new_tag('div', id="SMtext")
 					divtagimg = soup.new_tag('div', id="SMimgdiv")
@@ -315,42 +277,14 @@ class Command(BaseCommand): #2
 					divtag.append(newtag)
 					newtag.string = title_fin
 
+					newfile = soup.prettify()
+					print(newfile[-300:])
+					#turn a Beautiful Soup parse tree into a nicely formatted Unicode string, with each HTML/XML tag on its own line:
+				with open('templates/list_of_storymaps.html','w') as f:
+					f.write(newfile)
 
 
-					
-					#soup.ul.append(newtag)
-					#newtag.string= title_fin
-					#newtag.append(imgtag)
-					#newtag.wrap(soup.new_tag('li', id='SMListitem'))
-					#print soup
-					mynewfile = soup.prettify()
-				with open('templates/list_of_storymaps.html', 'w') as f:	
-					f.write(mynewfile)
-			
-def pb_start(entries, ns):
-	#determines whether a document has page breaks at the ends of entries or the 
-	#beginning of entries when an entry starts on a new page
-	#returns True if an element starts with a page break
-	for x in entries:
-		children = []
-		for child in x:
-			children.append(child)
-		if children[0].tag == ns +"pb":
-			return True
-	return False#18
 
-def page_repr(page_list):
-	#function to print page numbers (really just removes brackets)
-	page_string = str(page_list)
-	new = ""
-	for s in range(0,len(page_string)):
-		if page_string[s] != "[" and page_string[s] != "]":
-			new = new + page_string[s]
-	if len(page_list)== 1:
-		return "Page: " +new
-	else:	
-		return "Pages: "+new#19
-		
 def date_repr(date_str):
 	#ex: "1793-07-20"
 	#want to be : "July 20, 1793"
@@ -382,73 +316,16 @@ def date_repr(date_str):
 	else:
 		mo_fin = "December"
 	return mo_fin + " " + day +", " +year#20
-	
-def get_pages(page_list_a):
-		#shifting list of page breaks back one so it will be the list of page numbers
-	page_list_b = page_list_a #list of lists
-	#subtracts one from each page break associated with each div
-	for p in range(0, len(page_list_a)):
-		if page_list_a[p]!= []:
-			for x in range(0,len(page_list_a[p])):
-				page_list_b[p][x] = int(page_list_a[p][x]) - 1
 
-	#only deals with the beginning part
-	fill = [0]
-	if page_list_a[0] == []:
-		x = 0
-		while x < len(page_list_a) and page_list_a[x] == []:
-			x+=1
-		if x < len(page_list_a):
-			fill = [int(page_list_a[x][0])]
-		
-		for y in range(0,x):
-			page_list_b[y] = fill
-	else:
-		fill = page_list_a[0]
-		
-	#now need to do rest
-	
-	for n in range(0, len(page_list_a)):
-		if page_list_a[n] == []:
-			page_list_b[n].append(fill[0]+1)
-		else:
-			if len(page_list_a[n]) == 1:
-				fill = page_list_a[n]
-			else:
-				fill = [page_list_a[n][len(page_list_a[n])-1]]
-	return page_list_b#21
-	
-def get_pages_alt(page_list_a):
-	#get pages alternate is designed for refining the list of pages in
-	#cases where a page break might be at the beginning of an entry but we're not sure
-	#I am probably going to just not shift the numbers back
-	page_list_b = page_list_a #list of lists
-	for p in range(0, len(page_list_a)):
-		if page_list_a[p]!= []:
-			for x in range(0,len(page_list_a[p])):
-				page_list_b[p][x]= int(page_list_a[p][x]) #same except no minus 1
-				
-		#only deals with the beginning part
-	fill = [0]
-	if page_list_a[0] == []:
-		x = 0
-		while x < len(page_list_a) and page_list_a[x] == []:
-			x+=1
-		if x < len(page_list_a):
-			fill = [int(page_list_a[x][0])]
-		
-		for y in range(0,x):
-			page_list_b[y] = fill
-	else:
-		fill = page_list_a[0]
-	
-	#now need to do rest
-	for n in range(0, len(page_list_a)):
-		if page_list_a[n] == []:
-			page_list_b[n].append(int(fill[0])+1)
-		else:
-			if len(page_list_a[n]) == 1:
-				fill = page_list_a[n]
-			else:
-				fill = [page_list_a[n][len(page_list_a[n])-1]]
-	return page_list_b#22
+def media_url_repr(tei_ids_list):
+	tei_id = str(tei_ids_list)[2:]
+	tei_id = tei_id[:-2]
+	media_url="/ststic/img/"+tei_id+".jpg"
+	return media_url
+
+def page_repr(page_list):
+	#function to print page numbers (really just removes brackets)
+	pagenumber = str(page_list)
+	pagenumber = pagenumber[2:]
+	pagenumber = pagenumber[:-2]
+	return "Page: " +pagenumber
