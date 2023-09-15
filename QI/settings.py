298feb6ce06743b93a688067497b86f3005794f4
Alex Gleason <alex@alexgleason.me>
2023-09-15 09:29:42 -0500

    Add media settings and nginx conf, serve manuscript previews from media

--- QI/settings.py
+++ QI/settings.py
@@ -20,9 +20,11 @@ except:
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
